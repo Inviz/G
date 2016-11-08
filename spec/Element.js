5cd4fa0260a2b99eb6cea795a582d7764b2cf853
Yaroslaff Fedin <invizko@gmail.com>
2016-11-08 16:26:36 +0800

    Add batch spec for dom iterator

--- spec/Element.js
+++ spec/Element.js
@@ -543,7 +543,6 @@ describe('G.Node', function() {
         '<footer>Bye world</footer>' + 
       '</main>')
 
-    debugger
     last.call()
 
 
@@ -554,6 +553,77 @@ describe('G.Node', function() {
         '<article><h1>Title One</h1><hr><p></p></article>' + 
         '<article><h1>Title #2</h1><hr><p></p></article>' + 
         '<article><h1>Title #3</h1><hr><p></p></article>' + 
+        '<footer>Bye world</footer>' + 
+      '</main>')
+
+    context.items.set('title', 'Title Three')
+
+    expect(wrapper.render().outerHTML).to.eql(
+      '<main>' +
+        '<header>Hello world</header>' + 
+        '<article><h1>Title #0</h1><hr><p></p></article>' + 
+        '<article><h1>Title One</h1><hr><p></p></article>' + 
+        '<article><h1>Title #2</h1><hr><p></p></article>' + 
+        '<article><h1>Title Three</h1><hr><p></p></article>' + 
+        '<footer>Bye world</footer>' + 
+      '</main>')
+  })
+
+
+  it ('should build within iterator and batch render', function() {
+    var context = new G;
+    var item1 = new G({title: 'Title #1'})
+    var first = context.push('items', item1);
+    var second = context.push('items', {title: 'Title #2'});
+
+    var fragment = context.watch('items', function(item) {
+      return G.Node('article', null, 
+        G.Node('h1', null, item.title),
+        G.Node('hr'),
+        G.Node('p', null, item.text))
+    })
+
+    var wrapper = G.Node('main', null, 
+      G.Node('header', null, 'Hello world'),
+      fragment,
+      G.Node('footer', null, 'Bye world'))
+
+    expect(wrapper.render().outerHTML).to.eql(
+      '<main>' +
+        '<header>Hello world</header>' + 
+        '<article><h1>Title #1</h1><hr><p></p></article>' + 
+        '<article><h1>Title #2</h1><hr><p></p></article>' + 
+        '<footer>Bye world</footer>' + 
+      '</main>')
+
+    context.unshift('items', {title: 'Title #0'});
+    context.push('items', {title: 'Title #3'});
+    item1.set('title', 'Title One')
+    first.uncall()
+
+
+    var last = context.items.uncall();
+
+    expect(wrapper.render().outerHTML).to.eql(
+      '<main>' +
+        '<header>Hello world</header>' + 
+        '<article><h1>Title #0</h1><hr><p></p></article>' + 
+        '<article><h1>Title #2</h1><hr><p></p></article>' + 
+        '<footer>Bye world</footer>' + 
+      '</main>')
+
+    context.items.set('title', 'Title Two')
+    last.call()
+    first.call()
+    context.items.set('title', 'Title Three')
+
+    expect(wrapper.render().outerHTML).to.eql(
+      '<main>' +
+        '<header>Hello world</header>' + 
+        '<article><h1>Title #0</h1><hr><p></p></article>' + 
+        '<article><h1>Title One</h1><hr><p></p></article>' + 
+        '<article><h1>Title Two</h1><hr><p></p></article>' + 
+        '<article><h1>Title Three</h1><hr><p></p></article>' + 
         '<footer>Bye world</footer>' + 
       '</main>')
 
